db4f9a4f9fd05182378d074179483755574b8720
MUM <otaku.ict@gmail.com>
2024-11-11 16:53:24 +0700

    better version

--- Problem Sloving Patterns/unique values/unique_values.js
+++ Problem Sloving Patterns/unique values/unique_values.js
@@ -1,23 +1,17 @@
 function countUniqueValues(array = []) {
-  // add whatever parameters you deem necessary - good luck!
   let i = 0;
-  let new_arr = [];
-  if (array.length > 0) {
-    new_arr.push(array[0]);
-  }
 
   for (let j = 1; j < array.length; j++) {
     if (array[i] !== array[j]) {
       array[++i] = array[j];
-      new_arr.push(array[j]);
     }
   }
 
-  return new_arr.length;
+  return i + 1;
 }
 
 console.log(
   "🚀 ~ file: unique_values.js:13 ~ countUniqueValues:",
 
-  countUniqueValues([-2, -1, -1, 0, 1])
+  countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])
 );
